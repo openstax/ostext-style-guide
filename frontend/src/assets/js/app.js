bb0dab43b7e5bbf536eda0d1fe0f597f5e36389b
Zach Roehr <zach.roehr@gmail.com>
2017-04-17 23:40:10 -0500

    add sticky header, fix interactions with subsection and sticky header

--- frontend/src/assets/js/app.js
+++ frontend/src/assets/js/app.js
@@ -214,7 +214,7 @@ function heading(category, id, heading) {
 
     if (el) {
       let rect = el.getBoundingClientRect();
-      window.scrollTo(0, rect.top + pageYOffset - 20);
+      window.scrollTo(0, rect.top + pageYOffset - interactions.offsetValue());
     }
   }, 400);
 }
